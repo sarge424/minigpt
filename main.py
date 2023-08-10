21baa8e80ed427ece6b11e0de2e5aafcb971cb15
Alex Dawson <arjunsivakumar2@gmail.com>
2023-08-10 10:08:10 +0530

    bugs, cuda, saving

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import matplotlib.pyplot as plt
+import time
 
 # hyperparams
-batch_size = 16  # B
-block_size = 128  # T
-max_iters = 15000
-eval_iters = 500
-learning_rate = 1e-3
-
-n_embd = 32  # C
-n_head = 4
+batch_size = 64  # B
+block_size = 256  # T
+max_iters = 5000
+eval_iters = 200
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('running on', device)
+n_embd = 64*6      # C
+n_head = 6
 n_layer = 6
 dropout = 0.2
 
+model_filename = f'models/model{batch_size}-{block_size}-{n_embd}-{n_head}-{n_layer}-{max_iters}-{learning_rate:E}.pt'
+print('starting training for model', model_filename)
 torch.manual_seed(1337)
 
 # set input data
@@ -37,21 +40,22 @@ val_data = data[n:]
 
 
 # helper functions
+@torch.no_grad()
 def eval_model():
     out = {}
-    m.eval()
+    model.eval()
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
             X, Y = get_batch(split)
-            logits, loss = m(X, Y)
+            logits, loss = model(X, Y)
             losses[k] = loss.item()
 
             if k % (eval_iters/10) == 0:
                 print('.', end='')
         print('')
         out[split] = losses.mean()
-    m.train()
+    model.train()
     return out
 
 
@@ -60,7 +64,7 @@ def get_batch(split):
     ix = torch.randint(0, len(data) - block_size, (batch_size,))
     x = torch.stack([data[i: i + block_size] for i in ix])
     y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
-
+    x, y = x.to(device), y.to(device)
     return x, y
 
 
@@ -70,7 +74,7 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones((block_size, block_size))))
+        self.register_buffer('tril', torch.tril(torch.ones((block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
 
@@ -79,7 +83,7 @@ class Head(nn.Module):
         k = self.key(x)
         q = self.query(x)
 
-        wei = q @ k.transpose(-2, -1) * (C ** 0.5)
+        wei = q @ k.transpose(-2, -1) * (k.shape[-1] ** -0.5)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
 
@@ -94,7 +98,7 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(n_embd, n_embd)
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
@@ -140,10 +144,20 @@ class GPT(nn.Module):
         self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std = 0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std = 0.02)
+
     def forward(self, idx, targets=None):
         B, T = idx.shape
         token_emb = self.token_embedding_table(idx)  # B,T,C
-        pos_emb = self.position_embedding_table(torch.arange(T))  # T, C
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # T, C
         x = token_emb + pos_emb  # B,T,C + *1,T,C -> B,T,C
         x = self.blocks(x)
         x = self.ln_f(x)
@@ -173,14 +187,16 @@ class GPT(nn.Module):
 
 
 xb, yb = get_batch('train')
-m = GPT()
+model = GPT()
+m = model.to(device)
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+m.train()
 
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
-
-print('intial loss:', eval_model())
 losses = []
 lossi = []
+start = time.time()
 
 for i in range(max_iters):
     xb, yb = get_batch('train')
@@ -194,17 +210,21 @@ for i in range(max_iters):
 
     if i % (max_iters / 20) == 0:
         losses.append(torch.tensor(lossi).mean().item())
-        print(f'{i}/{max_iters} ({int(i*100/max_iters)}): {losses[-1]}')
-        lossi = []
-
-plt.plot(losses)
+        print(f'{i}/{max_iters} ({int(i*100/max_iters)}%: {int(time.time()-start)}sec so far): {losses[-1]}')
+    if i % (max_iters / 100) == 0:
+        print('.', end='')
 
 print('loss:', eval_model())
-context = torch.zeros((1, 1), dtype=torch.long)
+m.eval()
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=300)[0].tolist()))
 
-context = torch.zeros((1, 1), dtype=torch.long)
-out = decode(m.generate(context, max_new_tokens=10000)[0].tolist())
+print('saving model', model_filename, '...')
+torch.save(m.state_dict(), model_filename)
+print('saved.')
+
+print('writing output...')
+out = decode(m.generate(context, max_new_tokens=1000)[0].tolist())
 with open('output.txt', 'w') as file:
     file.write(out)
 
